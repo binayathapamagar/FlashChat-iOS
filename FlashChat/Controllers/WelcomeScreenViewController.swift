da47afbd1270c01631ff10020f9b22778ca4d867
Binaya Thapa Magar <binayaekacai96@gmail.com>
2021-07-28 01:09:03 +0545

    Added CLTypinglabel pod for the animation on the title

--- FlashChat/Controllers/WelcomeScreenViewController.swift
+++ FlashChat/Controllers/WelcomeScreenViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeScreenViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleLabel.text = "⚡️FLASH CHAT"
+        
     }
 
 
